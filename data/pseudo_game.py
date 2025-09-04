4b7fab6544f9e05a86b52a75bc58cd7fcd103560
kai <kaishun.teoh@gmail.com>
2025-09-04 01:37:39 +0100

    mf

--- data/pseudo_game.py
+++ data/pseudo_game.py
@@ -275,7 +275,7 @@ class PseudoGame:
         #     self.object.eaten = True 
         return 
     
-    def draw(self, plot = True):
+    def draw(self, plot = False):
         self.screen[:,:] = WHITE
 
         self.screen = self.player.draw(self.screen)
